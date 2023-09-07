a79a94465ce4ec2732e0ec8edc1deb3cc02df3f1
Nikita Titov <netitov@gmail.com>
2023-09-07 09:44:57 +0200

    update confin links

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
-const SERVER_API = 'http://api.wordslearner.site';
+const SERVER_API = 'https://api.wordslearner.site';
 const REQ_LIMIT = 800;
-const FRONT_LINK = 'http://wordslearner.site';
+const FRONT_LINK = 'https://wordslearner.site';
 
 module.exports = {
   SERVER_API, REQ_LIMIT, FRONT_LINK
